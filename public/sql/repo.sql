664a0b2703e6daac9366240838d3561e111668ec
khs1994 <khs1994@khs1994.com>
2018-05-07 11:45:45 +0800

    Update

--- public/sql/repo.sql
+++ public/sql/repo.sql
@@ -3,17 +3,17 @@ USE khsci;
 DROP TABLE IF EXISTS repo;
 
 CREATE TABLE `repo` (
-  `id`              BIGINT       AUTO_INCREMENT,
-  `git_type`        VARCHAR(20) COMMENT 'github coding gitee',
-  `rid`             BIGINT UNSIGNED,
-  `username`        varchar(100),
-  `repo_prefix`     VARCHAR(100),
-  `repo_name`       VARCHAR(100),
-  `repo_full_name`  VARCHAR(100),
-  `webhooks_status` INT UNSIGNED COMMENT 'Webhooks 是否开启  1 -> on    0 -> off',
-  `build_activate`  INT UNSIGNED COMMENT '是否打开构建       1 -> on    0 -> off',
-  `repo_admin`      JSON,
-  `default_branch`  VARCHAR(200) DEFAULT 'master',
-  `last_sync`       BIGINT UNSIGNED COMMENT '最后同步时间',
+  `id`                 BIGINT       AUTO_INCREMENT,
+  `git_type`           VARCHAR(20) COMMENT 'github coding gitee',
+  `rid`                BIGINT UNSIGNED,
+  `repo_prefix`        VARCHAR(100),
+  `repo_name`          VARCHAR(100),
+  `repo_full_name`     VARCHAR(100),
+  `webhooks_status`    INT UNSIGNED COMMENT 'Webhooks 是否开启  1 -> on    0 -> off',
+  `build_activate`     INT UNSIGNED COMMENT '是否打开构建       1 -> on    0 -> off',
+  `repo_admin`         JSON,
+  `repo_collaborators` JSON,
+  `default_branch`     VARCHAR(200) DEFAULT 'master',
+  `last_sync`          BIGINT UNSIGNED COMMENT '最后同步时间',
   KEY (`id`)
 );
