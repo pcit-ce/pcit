b752747c6297d5a64249ab854fae085bdcf5f6b8
khs1994 <khs1994@khs1994.com>
2018-05-13 10:50:21 +0800

    Update

--- public/app/Console/Up.php
+++ public/app/Console/Up.php
@@ -19,6 +19,7 @@ use KhsCI\Support\Git;
 use KhsCI\Support\HTTP;
 use KhsCI\Support\JSON;
 use KhsCI\Support\Log;
+use phpDocumentor\Reflection\DocBlock\Tags\Param;
 
 class Up
 {
@@ -407,17 +408,23 @@ EOF;
     {
         $obj = json_decode($content);
 
-        // $event_time = '';
-
         $action = $obj->action;
 
         $pull_request = $obj->pull_request;
 
-        $rid = $pull_request->base->repo->id;
+        $event_time = $pull_request->updated_at ?? $pull_request->created_at;
+
+        $event_time = Date::parse($event_time);
+
+        // head 向 base 提交 PR
+        $pull_request_base = $pull_request->base;
+        $pull_request_head = $pull_request->head;
+
+        $rid = $pull_request_base->repo->id;
 
         $commit_message = $pull_request->title;
 
-        $commit_id = $pull_request->head->sha;
+        $commit_id = $pull_request_head->sha;
 
         $committer_username = $pull_request->user->login;
 
