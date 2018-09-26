462837805caf942ad0663a1c8b857d9ccee11e5a
khs1994 <khs1994@khs1994.com>
2018-09-26 19:43:00 +0800

    Update frontend header [skip ci]
    Signed-off-by: khs1994 <khs1994@khs1994.com>

--- public/public/js/repos.js
+++ public/public/js/repos.js
@@ -1,4 +1,4 @@
-$('header').append(`<span class="ico">PCIT</span>
+$('header').append(`<span class="ico"><img title="PCIT IS A PHP CI TOOLKIT" id="pcit_ico" src="/ico/pcit.png"/></span>
 <span class="docs"><a href="//docs.ci.khs1994.com" target="_blank">Documentation</a></span>
 <span class="plugins"><a href="//docs.ci.khs1994.com/plugins/" target="_blank">Plugins</a></span>
 <span class="donate"><a href="//zan.khs1994.com" target="_blank">Donate</a></span>
